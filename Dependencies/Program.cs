c9bb03749af554be4b5de0f86917e9db31c11720
mihai <mihai@eessidev24>
2019-11-22 18:38:16 +0100

    Refactoring

--- Dependencies/Program.cs
+++ Dependencies/Program.cs
@@ -3,72 +3,85 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Dependencies
 {
     class Program
     {
-        static readonly Dictionary<string, AssemblyName> Dictionary = new Dictionary<string, AssemblyName>();
+        private static string InputPath;
+        private static string InputFilter;
+        private static readonly Dictionary<string, Assembly> Assemblies = new Dictionary<string, Assembly>();
+        private static readonly Dictionary<string, Assembly> InputAssemblies = new Dictionary<string, Assembly>();
+        private static readonly Dictionary<string, Assembly> Dependencies = new Dictionary<string, Assembly>();
 
-        static void Main(string[] assemblyPaths)
+        static void Main(string[] args)
         {
-            foreach (var assemblyPath in assemblyPaths)
+            InputPath = args[0];
+            InputFilter = args[1];
+
+            LoadAssemblies();
+
+            FilterAssemblies();
+
+            foreach (var inputAssembly in InputAssemblies.Values)
             {
-                var assembly = AssemblyName.GetAssemblyName(assemblyPath);
-                Add(assembly);
+                ComputeDependencies(inputAssembly);
             }
 
-            var dependencies = DependencyNames();
-            var input = FileNames(assemblyPaths);
-            var output = dependencies.Except(input);
-
+            var output = Dependencies.Except(InputAssemblies);
             Print(output);
         }
 
-        private static void Add(AssemblyName assemblyName)
+        private static void LoadAssemblies()
         {
-            if (Dictionary.ContainsKey(assemblyName.FullName))
+            foreach (var dll in Directory.GetFiles(InputPath, "*.dll"))
             {
-                return;
+                try
+                {
+                    var assembly = Assembly.LoadFile(dll);
+                    var filename = Path.GetFileName(dll);
+                    Assemblies[filename] = assembly;
+                }
+                catch { }
             }
+        }
 
-            Dictionary[assemblyName.FullName] = assemblyName;
+        private static void FilterAssemblies()
+        {
+            Regex regex = new Regex(InputFilter, RegexOptions.IgnoreCase);
+            foreach (var assemblyPath in Assemblies.Keys)
+            {
+                if (regex.IsMatch(assemblyPath))
+                {
+                    InputAssemblies[assemblyPath] = Assemblies[assemblyPath];
+                }
+            }
+        }
 
-            var dependencies = Dependencies(assemblyName);
+        private static void ComputeDependencies(Assembly assembly)
+        {
+            var dependencies = assembly.GetReferencedAssemblies();
             foreach (var dependency in dependencies)
             {
-                Add(dependency);
+                var dependencyPathAndAssembly = Assemblies.SingleOrDefault(x => x.Value.FullName == dependency.FullName);
+
+                if (!string.IsNullOrEmpty(dependencyPathAndAssembly.Key))
+                {
+                    if (!Dependencies.ContainsKey(dependencyPathAndAssembly.Key))
+                    {
+                        Dependencies[dependencyPathAndAssembly.Key] = dependencyPathAndAssembly.Value;
+                        ComputeDependencies(dependencyPathAndAssembly.Value);
+                    }
+                }
             }
         }
 
-        private static AssemblyName[] Dependencies(AssemblyName assemblyName)
+        private static void Print(IEnumerable<KeyValuePair<string, Assembly>> dependencies)
         {
-            try
+            foreach (var dependency in dependencies)
             {
-                var assembly = Assembly.Load(assemblyName);
-                return assembly.GetReferencedAssemblies();
-            }
-            catch
-            {
-                return new AssemblyName[0];
-            }
-        }
-
-        private static List<string> DependencyNames()
-        {
-            return Dictionary.Values.Select(a => a.Name).Distinct().ToList();
-        }
-
-        private static List<string> FileNames(IEnumerable<string> files)
-        {
-            return files.Select(Path.GetFileNameWithoutExtension).ToList();
-        }
-
-        private static void Print(IEnumerable<string> assemblyPaths)
-        {
-            foreach (var assemblyPath in assemblyPaths)
-            {
-                Console.WriteLine(assemblyPath);
+                Console.WriteLine(dependency.Key);
             }
         }
     }
