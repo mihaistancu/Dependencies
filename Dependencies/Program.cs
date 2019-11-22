f9be82abb45163a7499b7b14c078c4b3b938b6c0
mihai <mihai@eessidev24>
2019-11-22 12:09:12 +0100

    Empty console application

---
+++ Dependencies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dependencies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Dependencies");
+        }
+    }
+}
